6842ff85518a8bc2e549b5d8ab68afb25ee40181
lepronet <lepronet85@gmail.com>
2024-09-18 16:15:12 +0100

    Update family routes

--- server/routes/family.routes.js
+++ server/routes/family.routes.js
@@ -1,8 +1,19 @@
-const FamilyController = require("../controllers/family.controllers");
+const {
+  createFamily,
+  getAllFamilies,
+  getFamilyById,
+  updateFamily,
+  deleteFamily,
+} = require("../controllers/family.controllers");
 
 const express = require("express");
 const router = express.Router();
 
-router.get("/", FamilyController);
+router.post("/family", createFamily);
+router.get("/family", getAllFamilies);
+router.get("/family/:id", getFamilyById);
+router.put("/family/:id", updateFamily);
+router.delete("/family/:id", deleteFamily);
+router.get("/family/:id/members", getFamilyMembers);
 
 module.exports = router;
